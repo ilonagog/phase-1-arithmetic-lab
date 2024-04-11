33646f12f257ef7564d6bde1f9b92c211dcb064f
ilona <gogiashvili.ilona@gmail.com>
2024-04-11 11:37:49 -0400

    commit tree ae380296cfa2996b5af171948019e1342126ea28

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 1000) + 1;
+// let random = Math.random();
+let num3 = 16;
+let num4 = 6;
+let mod = num3 % num4;
+
+let max = Math.max(1, 3, 20);
